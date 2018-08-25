caa11c22740eb3a05db0dcae6132e88c1dd07dec
Igor Kuznetsov <igk@igk.ru>
2018-08-26 00:23:37 +0300

    Template path do merge with exists paths.

--- admin-customize.php
+++ admin-customize.php
@@ -45,7 +45,7 @@ class AdminCustomizePlugin extends Plugin
      */
     public function onAdminTwigTemplatePaths($event)
     {
-        $event['paths'] = [__DIR__ . '/admin/themes/grav/templates'];
+        $event['paths'] = array_merge($event['paths'], [__DIR__ . '/admin/themes/grav/templates']);
         //$this->grav['twig']->twig_paths[] = __DIR__ . '/admin/themes/grav/templates';
     }
 
